77187d41b39130c4ee679c6c020b036fd32807e6
EJIKE <perpetualezeifeanyi@gmail.com>
2022-08-19 23:45:49 +0100

    stored data in the our database

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 require("dotenv").config()
 const mongoose = require('mongoose');
-const db = "mongodb://localhost:27017"
+const db ="mongodb+srv://bookclub:%40OxBrando0@cluster0.hmj5qle.mongodb.net/?retryWrites=true&w=majority"
 
 module.exports = function () {
     mongoose.connect(db, {
