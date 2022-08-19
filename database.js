2e51072b508f2872c0e462041a7701c0db3d954d
EJIKE <perpetualezeifeanyi@gmail.com>
2022-08-19 23:57:10 +0100

    had to hide some credenticals

--- database.js
+++ database.js
@@ -1,7 +1,6 @@
 require("dotenv").config()
 const mongoose = require('mongoose');
-const db ="mongodb+srv://bookclub:%40OxBrando0@cluster0.hmj5qle.mongodb.net/?retryWrites=true&w=majority"
-
+const db = process.env.DB
 module.exports = function () {
     mongoose.connect(db, {
         useNewUrlParser: true,
