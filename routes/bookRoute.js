c45b9da39700ce5cb9b92d1044bb7acd41d6456d
EJIKE <perpetualezeifeanyi@gmail.com>
2022-08-20 01:18:35 +0100

    worked on forget password views and route

--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -1,16 +1,26 @@
 const bookRoute = require('express').Router()
-
 const { getAllBooks, createBook, getBook, updateBook, deleteBook } = require('../controllers/bookController')
 
-bookRoute.get('/', getAllBooks)
-bookRoute.post('/', createBook)
+bookRoute.get('/list', getAllBooks)
+bookRoute.post('/create',  createBook)
 bookRoute.get('/:id', getBook)
-bookRoute.patch('/:id', updateBook)
-bookRoute.delete('/:id', deleteBook)
+bookRoute.patch('/:id',  updateBook)
+bookRoute.delete('/:id',  deleteBook)
 
 bookRoute.get('/check', (req, res) => {
     res.send('BOOKS AVAILABLE')
 })
 
+// function checkAuthenticated(req, res, next) {
+//     if (req.book) {
+//         next();
+//     }
+
+//     else {
+//         req.flash('success_msg', 'You need to be authenticated to access this page');
+//         res.redirect("/login");
+
+//     }
+// }
 module.exports = bookRoute
 
